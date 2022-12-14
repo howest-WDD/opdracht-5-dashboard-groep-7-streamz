b776063ac21f74f69b48e5459c062c9b43e7e72d
VanduyfhuysYanis <yanis.vanduyfhuys@student.howest.be>
2022-12-14 15:21:07 +0100

    table info loaded with json

---
+++ js/gebruikers.js
@@ -0,0 +1,46 @@
+import data from '../json/users.json';
+let count = 1;
+
+const showGebruikers = function () {
+    let tableHtml='';
+    for(const dataUsers of data.users){
+      const lastName = dataUsers.lastName;
+      const firstName = dataUsers.firstName;
+      const img = dataUsers.image;
+      const email = dataUsers.email;
+      const type = dataUsers.type;
+      const gebdat = dataUsers.dateOfBirth;
+      const geslacht = dataUsers.gender;
+
+      tableHtml+=`
+      <tr>
+          <td>
+            <label class="c-contain">
+            <input type="checkbox" />
+            <div class="c-input"></div>
+          </label>
+        </td>
+        <td>`+count+`</td>
+        <td><img src="`+img+`" alt="`+firstName+` `+lastName+`"></td>
+        <td>`+firstName+` `+lastName+`</td>
+        <td>`+email+`</td>
+        <td>`+type+`</td>
+        <td>`+gebdat+`</td>
+        <td>`+geslacht+`</td>
+        </tr>
+        <tr>
+      `;
+      count++;
+    }
+
+    document.querySelector('.js-table__body').innerHTML = tableHtml;
+
+};
+
+
+const init = function () {   
+    showGebruikers();
+};
+
+document.addEventListener("DOMContentLoaded", init);
+  
