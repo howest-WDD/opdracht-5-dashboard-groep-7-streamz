88f22fb84b3e532cef2588337f00bf779241b57a
milandepauw <milan.de.pauw@student.howest.be>
2022-12-20 21:02:45 +0100

    navigatie v0.2

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,13 @@
 function showTime() {
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
 
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
 
-    var time = h + ":" + m;
+    let time = h + ":" + m;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
 
@@ -15,15 +15,22 @@ function showTime() {
 
 }
 
-showTime();
+
 
 
 
 
 
 function ShowDate() {
-    var newdate = new Date().toLocaleDateString('nl-be', { year:"numeric", weekday:"short", month:"long", day:"numeric"});
+    let newdate = new Date().toLocaleDateString('nl-be', { year:"numeric", weekday:"short", month:"long", day:"numeric"});
 
     document.getElementById('MyDateDisplay').innerHTML = newdate;
 }
-ShowDate();
+
+
+const init = function () {
+    showTime();
+    ShowDate();
+};
+
+  document.addEventListener("DOMContentLoaded", init);
