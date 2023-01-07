010d20fefbb2d01bfca3f52d40884809172dbd4f
sammussly <sam.cornflakes1998@gmail.com>
2023-01-07 16:02:04 +0100

    add tabs

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const path = require("path");
 }),
   (module.exports = {
     mode: "production",
-    entry: ["./js/test.js", "./js/clock.js", "./js/chartgebruikers.js", "./js/chartkijkuren.js", "./js/chartsubs.js", "./node_modules/chart.js/dist/chart.umd.js", "./js/dbdropdown.js", "./js/belgium.js"],
+    entry: ["./js/test.js", "./js/clock.js", "./js/chartgebruikers.js", "./js/chartkijkuren.js", "./js/chartsubs.js", "./node_modules/chart.js/dist/chart.umd.js", "./js/dbdropdown.js", "./js/belgium.js", "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"],
     output: {
       filename: "main.js",
       path: path.resolve(__dirname, "dist"),
     },
   });
-
