a5a6b14974250c629ca99e64b34a7a426e933f33
VanduyfhuysYanis <yanis.vanduyfhuys@student.howest.be>
2023-01-08 17:35:06 +0100

    form avatar/ form valid/ clock.js comment

---
+++ js/formavatar.js
@@ -0,0 +1,48 @@
+const formUpload = function () {
+
+const UPLOAD_BUTTON = document.getElementById("upload-button");
+const FILE_INPUT = document.querySelector("input[type=file]");
+const AVATAR = document.getElementById("js-avatar");
+
+
+UPLOAD_BUTTON.addEventListener("click", () => FILE_INPUT.click());{
+  FILE_INPUT.addEventListener("change", event => {
+  const file = event.target.files[0];
+
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onloadend = () => {
+    AVATAR.setAttribute("aria-label", file.name);
+    AVATAR.style.background = `url(${reader.result}) center center/cover`;
+    UPLOAD_BUTTON.classList.add("c-form__preview--hide");
+  };
+});
+}
+
+
+AVATAR.addEventListener("click", () => FILE_INPUT.click());
+
+FILE_INPUT.addEventListener("change", event => {
+  const file = event.target.files[0];
+
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onloadend = () => {
+    AVATAR.setAttribute("aria-label", file.name);
+    AVATAR.style.background = `url(${reader.result}) center center/cover`;
+  };
+});
+
+
+}
+
+const init = function () {
+
+    if (document.querySelector(".js-form")) {
+        formUpload();
+    }
+  };
+  
+document.addEventListener("DOMContentLoaded", init);
