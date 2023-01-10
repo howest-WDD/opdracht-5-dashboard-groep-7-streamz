465c632ef1ffbea8c315a7bf6439bc07c47cba7a
milandepauw <milan.de.pauw@student.howest.be>
2023-01-11 00:59:29 +0100

    startschermbeheer-v0.1

---
+++ js/startscherm.js
@@ -0,0 +1,39 @@
+window.onload=function(){
+    const form = document.getElementById("form");
+    const output = document.getElementById("output");
+    let divs = [];
+    let count = 1;
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+    
+        const div = document.createElement("tr");
+        div.className = "c-sbtable__row";
+    
+        div.innerHTML = `
+        <td>${count}</td>
+        <td>${name}</td>
+        `;
+        divs.push(div);
+    
+    
+        while (output.firstChild) {
+            output.removeChild(output.firstChild);
+        }
+    
+    
+        divs.forEach((div, index) => {
+            output.appendChild(div);
+        });
+        form.reset();
+        count++;
+    });
+    
+    $("#output").sortable({
+        update: function (event, ui) {
+            $("#output > tr").each(function (index) {
+                $(this).find('td:first-of-type').text(`${index + 1}`);
+            });
+        }
+    });
+}
