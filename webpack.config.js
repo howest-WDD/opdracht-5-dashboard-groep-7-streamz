17da32dc8a5353bcf98ce64a8160799628052411
VanduyfhuysYanis <yanis.vanduyfhuys@student.howest.be>
2023-01-19 17:26:50 +0100

    chart and hours added to detail

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 }),
   (module.exports = {
     mode: "production",
-    entry: ["./js/test.js", "./js/clock.js","./js/addnav.js","./js/startscherm.js","./js/chartgebruikers.js", "./js/chartkijkuren.js", "./js/chartsubs.js", "./node_modules/chart.js/dist/chart.umd.js", "./js/dbdropdown.js", "./js/belgium.js", "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", "./js/formvalid.js", "./js/formavatar.js", "./js/multiselectdropdown.js", "./js/table.js"],
+    entry: ["./js/test.js", "./js/clock.js","./js/addnav.js","./js/startscherm.js","./js/chartgebruikers.js", "./js/chartkijkuren.js", "./js/chartsubs.js", "./node_modules/chart.js/dist/chart.umd.js", "./js/dbdropdown.js", "./js/belgium.js", "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", "./js/formvalid.js", "./js/formavatar.js", "./js/multiselectdropdown.js", "./js/table.js", "./js/charthours.js"],
     output: {
       filename: "main.js",
       path: path.resolve(__dirname, "dist"),
